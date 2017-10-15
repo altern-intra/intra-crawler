719818f2d7b945c1428382ca58f4e3fbe1a6cc0f
Wats0ns <quentin.maire@protonmail.ch>
2017-10-15 15:23:37 +0200

    You can now refresh events from the planning, and they will be added to a mongo collection

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const Intranet = require('intra-api');
+var mongoose = require('mongoose');
+
+const Planning = require('./lib/planning')
+
+// Connection configuration
+const Intra = new Intranet(process.env.AUTOLOGIN_TOKEN);
+mongoose
+.connect(`mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DB}`, {
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PASSWORD
+})
+.then(() => {
+  // Models
+  const conn = mongoose.connection,
+    models = {
+      event: conn.model('Event', require('./models/event'))
+    }
+  console.log(`Connected to ${process.env.MONGO_DB}@${process.env.MONGO_HOST}`)
+  const planning = new Planning(Intra, models)
+  planning.refresh()
+  .then((err, res) => {
+    console.log('ya')
+  })
+})
+.catch((err) => {
+  throw err
+});
---
+++ models/event.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const Event = new mongoose.Schema({
+  scolaryear: 'string'
+})
+
+module.exports = Event;
